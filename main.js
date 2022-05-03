107050357b11f57d982a4df96ed35d2ec8d461f5
Neelutpal Saha <neelutpal.saha@s.amity.edu>
2022-05-03 10:32:50 +0000

    add game mechanics

--- main.js
+++ main.js
@@ -15,9 +15,27 @@ function game(playerSelection) {
 
   computerSign.textContent = signs[computerSelection];
   playerSign.textContent = signs[playerSelection];
+
+  let state = playRound(playerSelection, computerSelection);
 }
 
 function computerPlay() {
   let choices = ["rock", "paper", "scissors"];
   return choices[Math.floor(Math.random() * choices.length)];
+}
+
+function playRound(playerSelection, computerSelection) {
+  if (playerSelection === computerSelection) return "tie";
+  else if (playerSelection === "rock" && computerSelection === "paper")
+    return "lost";
+  else if (playerSelection === "rock" && computerSelection === "scissors")
+    return "won";
+  else if (playerSelection === "paper" && computerSelection === "scissors")
+    return "lost";
+  else if (playerSelection === "paper" && computerSelection === "rock")
+    return "won";
+  else if (playerSelection === "scissors" && computerSelection === "rock")
+    return "lost";
+  else if (playerSelection === "scissors" && computerSelection === "paper")
+    return "won";
 }
